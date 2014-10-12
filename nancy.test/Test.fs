79dd7459d1c2ecb6f3156abd62d392d5472a8c75
Mark Schultz <mxs802@case.edu>
2014-10-12 12:07:46 -0500

    add test

--- nancy.test/Test.fs
+++ nancy.test/Test.fs
@@ -2,9 +2,13 @@
 
 open System
 open NUnit.Framework
+open Nancy.Testing
+open FsUnit
+open FsCheck
 
 [<TestFixture>]
 type Test() = 
     [<Test>]
     member x.TestCase() = Assert.IsTrue(true)
-
+    [<Test>]
+    member x.Test2() = "ships" |> should startWith "sh"
