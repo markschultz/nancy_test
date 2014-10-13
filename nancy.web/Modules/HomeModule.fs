a8498366a94d4a7f90057af1a0490e34f1767afe
Mark Schultz <mxs802@case.edu>
2014-10-12 23:23:05 -0500

    update test pages and layouts

---
+++ nancy.web/Modules/HomeModule.fs
@@ -0,0 +1,9 @@
+﻿namespace nancy_test
+open Nancy
+
+type HomeModule() as x =
+    inherit NancyModule()
+    let IsPjax = false
+    do x.Get.["/home/"] <- fun _ -> box x.View.["HomeIndex"]
+    do x.Get.["/home/first/"] <- fun _ -> box x.View.["First"]
+    do x.Get.["/home/second/"] <- fun _ -> box x.View.["Second"]
